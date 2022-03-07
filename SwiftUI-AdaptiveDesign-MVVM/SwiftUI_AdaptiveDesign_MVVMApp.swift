a21bfd387c40b2e53065e84bbbf619819b135553
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2022-03-08 00:06:05 +0300

    Initial Commit

---
+++ SwiftUI-AdaptiveDesign-MVVM/SwiftUI_AdaptiveDesign_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AdaptiveDesign_MVVMApp.swift
+//  SwiftUI-AdaptiveDesign-MVVM
+//
+//  Created by Alexander Sobolev on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AdaptiveDesign_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
